e5ade7d10e7d179d3540b03f735d90f05d98b36d
Aakash Jha <aakash.jha1103@gmail.com>
2021-08-22 20:08:29 +0530

    Directory refactoring

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ require("dotenv/config")
 const { Client, Intents } = require("discord.js")
 const client = new Client({ intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS] })
 
-const commandHandler = require("./commandHandler")
+const commandHandler = require("./utils/commandHandler")
 
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag} :)`)
